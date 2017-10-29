96966bb81c3ca1381e93427348f7977d1edfedf2
Lucas Sales <saless182@gmail.com>
2017-10-29 20:55:48 -0200

    cleanup

--- app/build/tmp/kapt3/stubs/debug/com/trinus/suffragium/manager/AppManager.java
+++
@@ -1,8 +0,0 @@
-package com.trinus.suffragium.manager;
-
-@kotlin.Metadata(mv = {1, 1, 7}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/trinus/suffragium/manager/AppManager;", "", "init", "", "application", "Landroid/app/Application;", "app_debug"})
-public abstract interface AppManager {
-    
-    public abstract void init(@org.jetbrains.annotations.NotNull()
-    android.app.Application application);
-}
